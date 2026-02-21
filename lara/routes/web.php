17c829dcf1ccc02db318d6368e24e255a4ffeaf4
Lylow <petooh108@gmail.com>
2026-02-21 12:23:44 +0500

    make route

--- lara/routes/web.php
+++ lara/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\AdminController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Публичные маршруты
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::post('/booking', [BookingController::class, 'store'])->name('booking.store');
+Route::post('/booking/{booking}/payment', [BookingController::class, 'payment'])->name('booking.payment');
+
+// Админ-панель (в реальном проекте добавьте middleware)
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [AdminController::class, 'dashboard'])->name('dashboard');
+    Route::post('/skates', [AdminController::class, 'storeSkate'])->name('skates.store');
+    Route::put('/skates/{skate}', [AdminController::class, 'updateSkate'])->name('skates.update');
+    Route::delete('/skates/{skate}', [AdminController::class, 'deleteSkate'])->name('skates.delete');
 });
